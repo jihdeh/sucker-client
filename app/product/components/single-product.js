3614e62a84e883d588a802cae399fe4e6bb0d4d1
jihdeh <jide.b.tade@gmail.com>
2017-01-11 02:01:41 +0100

    upgrade antd for update on input

--- app/product/components/single-product.js
+++ app/product/components/single-product.js
@@ -1,13 +1,14 @@
-import React, {PropTypes} from "react";
+import React, { PropTypes } from "react";
 import IPropTypes from "react-immutable-proptypes";
 import compose from "recompose/compose";
 import lifecycle from "recompose/lifecycle";
 import setPropTypes from "recompose/setPropTypes";
 import onlyUpdateForPropTypes from "recompose/onlyUpdateForPropTypes";
 import setDisplayName from "recompose/setDisplayName";
+import withState from "recompose/withState";
 import withHandlers from "recompose/withHandlers";
 import { connect } from "react-redux";
-import {Row, Col } from "antd";
+import { Row, Col, Input, Icon } from "antd";
 
 
 
@@ -19,30 +20,50 @@ const enhance = compose(
 	setDisplayName("SingleProduct"),
 	onlyUpdateForPropTypes,
 	setPropTypes({
-		productList: IPropTypes.map
+		productList: IPropTypes.map,
+		emitEmpty: PropTypes.func,
+		onChangeSearchTerm: PropTypes.func,
+		searchTerm: PropTypes.string,
 	}),
-	lifecycle({
-    componentDidMount: function() {
-     // console.log('props', this.props);
-    },
-    componentWillReceiveProps: function (newProps) {
-     // console.log('new props', newProps);
-    }
-  }),
+	withState('searchTerm', 'onSearchTermEntered', ''),
+	withHandlers({
+		emitEmpty: props => () => {
+			props.searchInputNode.focus();
+			props.onSearchTermEntered('');
+			// this.setState({ userName: '' });
+		},
+		onChangeSearchTerm: props => (e) => {
+			props.onSearchTermEntered(e.target.value);
+			// this.setState({ userName: e.target.value });
+		}
+	})
 );
 
 const SingleProduct = enhance(({
-	productList = new Map()
+	productList = new Map(),
+	onChangeSearchTerm,
+	searchTerm,
+	emitEmpty,
+	searchInputNode,
 }) => {
 	const product = Object.assign({}, productList.toJS());
+	const suffix = searchTerm ? <Icon type="close-circle" onClick={emitEmpty} /> : null;
 	return (
 		<div>
 			<Row>
-				<Col xs={6} sm={8} md={0} lg={0} className="mobile-product-assets">
+				<Col xs={24} sm={24} md={0} lg={0} className="mobile-product-assets">
 					<div className="mobile-product-lead">
 						<p>Hello</p>
 					</div>
 					<div className="mobile-product-search">
+						<Input
+							placeholder="Search our entire store"
+							addonBefore={<Icon type="search" />}
+							suffix={suffix}
+							value={searchTerm}
+							onChange={onChangeSearchTerm}
+							ref={node => searchInputNode = node}
+						/>
 					</div>
 					<div className="mobile-product-cart">
 
